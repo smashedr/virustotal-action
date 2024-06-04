3f8337fa7211671221022644b47d250d4f032adf
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-04 15:00:17 -0700

    links

--- dist/index.js
+++ dist/index.js
@@ -31089,14 +31089,13 @@ const github = __nccwpck_require__(5438)
 
         // console.log('github.context', github.context)
 
-        console.log('github.context.eventName:', github.context.eventName)
         if (github.context.eventName !== 'release') {
             console.log('Not a release:', github.context.eventName)
             // return
         }
 
-        const testInput = core.getInput('test_input')
-        console.log('testInput:', testInput)
+        const vtApiKey = core.getInput('vt_api_key')
+        console.log('vtApiKey:', vtApiKey)
 
         const githubToken = core.getInput('github_token')
         console.log('githubToken:', githubToken)
@@ -31108,12 +31107,17 @@ const github = __nccwpck_require__(5438)
         console.log('releaseTag:', releaseTag)
         const testTag = '0.1.12'
         console.log('testTag:', testTag)
+
         const release = await octokit.rest.repos.getReleaseByTag({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             tag: testTag,
         })
         // console.log('release:', release)
+        if (!release?.data) {
+            console.log('Release Not Found:', release)
+            return
+        }
 
         const assets = await octokit.rest.repos.listReleaseAssets({
             owner: github.context.repo.owner,
@@ -31121,6 +31125,10 @@ const github = __nccwpck_require__(5438)
             release_id: release.data.id,
         })
         // console.log('assets:', assets)
+        if (!assets.data?.length) {
+            console.log('No Assets Found:', assets)
+            return
+        }
 
         const links = getAssetsLinks(assets.data)
         console.log('links:', links)
@@ -31138,10 +31146,10 @@ const github = __nccwpck_require__(5438)
  * @return {Array[String]}
  */
 function getAssetsLinks(assets) {
-    console.log('assets:', assets)
+    // console.log('assets:', assets)
     const links = []
     assets.forEach((asset) => {
-        console.log('asset:', asset)
+        // console.log('asset:', asset)
         links.push(asset.browser_download_url)
     })
     return links
--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const github = require('@actions/github')
 
         // console.log('github.context', github.context)
 
-        console.log('github.context.eventName:', github.context.eventName)
         if (github.context.eventName !== 'release') {
             console.log('Not a release:', github.context.eventName)
             // return
         }
 
-        const testInput = core.getInput('test_input')
-        console.log('testInput:', testInput)
+        const vtApiKey = core.getInput('vt_api_key')
+        console.log('vtApiKey:', vtApiKey)
 
         const githubToken = core.getInput('github_token')
         console.log('githubToken:', githubToken)
@@ -28,12 +27,17 @@ const github = require('@actions/github')
         console.log('releaseTag:', releaseTag)
         const testTag = '0.1.12'
         console.log('testTag:', testTag)
+
         const release = await octokit.rest.repos.getReleaseByTag({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             tag: testTag,
         })
         // console.log('release:', release)
+        if (!release?.data) {
+            console.log('Release Not Found:', release)
+            return
+        }
 
         const assets = await octokit.rest.repos.listReleaseAssets({
             owner: github.context.repo.owner,
@@ -41,6 +45,10 @@ const github = require('@actions/github')
             release_id: release.data.id,
         })
         // console.log('assets:', assets)
+        if (!assets.data?.length) {
+            console.log('No Assets Found:', assets)
+            return
+        }
 
         const links = getAssetsLinks(assets.data)
         console.log('links:', links)
@@ -58,10 +66,10 @@ const github = require('@actions/github')
  * @return {Array[String]}
  */
 function getAssetsLinks(assets) {
-    console.log('assets:', assets)
+    // console.log('assets:', assets)
     const links = []
     assets.forEach((asset) => {
-        console.log('asset:', asset)
+        // console.log('asset:', asset)
         links.push(asset.browser_download_url)
     })
     return links
