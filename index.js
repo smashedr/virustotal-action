7e5552127865616c8dabe1e85e90ab49de29c71f
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-04 13:34:01 -0700

    Test

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log('github.context.payload', github.context.payload)
+;(async () => {
+    try {
+        // Get the JSON webhook payload for the event that triggered the workflow
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log('github.context.payload', github.context.payload)
 
-    console.log('github.context', github.context)
+        console.log('github.context', github.context)
 
-    const githubToken = core.getInput('githubToken')
+        const githubToken = core.getInput('githubToken')
 
-    const octokit = github.getOctokit(githubToken)
-    console.log('octokit', octokit)
+        const octokit = github.getOctokit(githubToken)
+        console.log('octokit', octokit)
 
-    console.log('github.context.ref', github.context.ref)
-    console.log('using test value:', '0.1.12')
-    const release = await octokit.rest.repos.getReleaseByTag(
-        ...github.context.repo,
-        '0.1.12'
-    )
-    console.log('release', release)
+        console.log('github.context.ref', github.context.ref)
+        console.log('using test value:', '0.1.12')
+        const release = await octokit.rest.repos.getReleaseByTag(
+            ...github.context.repo,
+            '0.1.12'
+        )
+        console.log('release', release)
 
-    const assets = await octokit.rest.repos.listReleaseAssets({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        release,
-    })
-    console.log('assets', assets)
+        const assets = await octokit.rest.repos.listReleaseAssets({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            release,
+        })
+        console.log('assets', assets)
 
-    // core.setOutput("time", time);
-} catch (error) {
-    core.setFailed(error.message)
-}
+        // core.setOutput("time", time);
+    } catch (error) {
+        console.warn(error)
+        core.setFailed(error.message)
+    }
+})()
