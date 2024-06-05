9dfc817bba249804f401ac4316024e25440c5f93
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-04 21:05:30 -0700

    More Cleanup

--- dist/index.js
+++ dist/index.js
@@ -38166,7 +38166,23 @@ async function downloadAsset(asset, assetsDir = 'assets') {
         responseType: 'stream',
     })
     // console.log('response:', response)
-    response.data.pipe(fs.createWriteStream(filePath))
+
+    // TODO: Cleanup this code
+    // response.data.pipe(fs.createWriteStream(filePath))
+    await new Promise((resolve, reject) => {
+        const writer = fs.createWriteStream(filePath)
+        response.data.pipe(writer)
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+    })
+
+    // const writer = fs.createWriteStream(filePath)
+    // response.data.pipe(writer)
+    // await new Promise((resolve, reject) => {
+    //     writer.on('finish', resolve)
+    //     writer.on('error', reject)
+    // })
+
     console.log('wrote:', filePath)
     return filePath
 }
